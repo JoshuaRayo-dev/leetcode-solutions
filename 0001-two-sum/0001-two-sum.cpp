e317b73a44edc9fc9bbc0e678a1191bd36de287f
Joshua Rayo <joshuarayo14@hotmail.com>
2026-09-09 16:02:01 -0600

    0001-TWO-SUM Stats: Time: 0 ms (100%), Space: 15.3 MB (8.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hashMap;
-        vector<int> res(2);
-
-        for (int i = 0; i < nums.size(); ++i) {
-            hashMap[nums[i]] = i;
-            // el mapa guarda [numero, index]
-        }
-
+        hashMap.reserve(nums.size());
+        
         for (int i = 0; i < nums.size(); ++i) {
             int x = nums[i];
             int y = target - x;
-
             auto it = hashMap.find(y);
 
-            if (it != hashMap.end() && i != hashMap.at(y)) {
-                res[0] = i;
-                res[1] = hashMap.at(y);
-                break;
+            if (it != hashMap.end()) {
+                return {i, it->second};
             }
+
+            hashMap[x] = i;
         }
 
-        return res;
+        return {};
     }
 };
